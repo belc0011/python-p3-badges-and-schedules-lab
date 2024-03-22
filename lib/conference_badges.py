1604b117b32812ba8990a18e26f5344c7fb17a1c
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-22 08:21:32 -0700

    complete badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    list_of_names = []
+    for name in names:
+        greeting = badge_maker(name)
+        list_of_names.append(greeting)
+    return list_of_names
 
 def assign_rooms(names):
-    return None
+    counter = 1
+    new_list = []
+    while counter < 9:
+        for name in names:
+            greeting = f"Hello, {name}! You'll be assigned to room {counter}!"
+            new_list.append(greeting)
+            counter +=1
+    return new_list
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
